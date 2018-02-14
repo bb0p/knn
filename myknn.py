4d4b171f4c523d5d16c09f75d97a95e8e3b40a62
bb0p <b@b0p.us>
2018-02-13 20:39:52 -0500

    second

--- myknn.py
+++ myknn.py
@@ -1,50 +1,39 @@
 from numpy import *
 import glob
+
+k=10
+
 f=glob.glob('features/*.npy')
 v=load(f[0])
 v=zeros((len(v),len(f)))
-for i in range(len(f)):
-	v[:,i]=load(f[i]).flatten()
+for i in range(len(f)): v[:,i]=load(f[i]).flatten()
 x=[]
 r=0
 for i in range(len(v)):
 	l=[]
-	for j in range(10):
+	for j in range(k):
 		r=random.randint(len(v))
 		while r==i:
 			r=random.randint(len(v))
 		l.append(r)	
 	x.append(l)	
-for epoch in range(10):
-	oldc=10000000
-	c=oldc-1
-	while c<oldc:
-		s=0
-		oldc=c
-		c=0
-		for i in range(len(x)):
-			l=[]
-			for j in range(len(x[i])):
-				l.extend(x[x[i][j]])
-			l=list(sorted(set(l)))
-				
-			j=0
-			while j<len(l):
-				if l[j]==i: 
-					l.pop(j)
-				else:
-					j+=1
-			old=sum((v[i]-v[x[i][epoch]])**2)
-			new=0
-			newj=i
-			for j in range(len(l)):
-				new=mean((v[i]-v[l[j]])**2)
-				if new<old:
-					old=new
-					newj=j
-			x[i].pop()
-			if newj<>i:
-				x[i].insert(epoch,l[newj])
-				c+=1
-			s+=mean((v[i]-v[l[newj]])**2)
-		print epoch,s/i,c,oldc
+total_mse=ones(len(x))
+old_mse=mean(total_mse)+1
+e=0
+while mean(total_mse)<old_mse:
+	old_mse=mean(total_mse)
+	for i in range(len(x)):
+		l=list(x[i])
+		for j in range(len(x[i])): l.extend(x[x[i][j]])
+		l=list(set(filter(lambda a: a!=i,l)))
+		mse=zeros(len(l))		
+		for j in range(len(l)):	mse[j]=mean((v[i]-v[l[j]])**2)
+		sort_mse=argsort(mse)
+		del x[i]
+		x.insert(i,[])
+		for j in range(k): x[i].append(l[sort_mse[j]])
+		total_mse[i]=mean(sort(mse)[0:k])
+		if random.randint(10000)==0: print e,i,mean(total_mse),old_mse-mean(total_mse)
+	print e,i,mean(total_mse),old_mse-mean(total_mse)
+	print '---------------------------------------------'
+	e+=1
