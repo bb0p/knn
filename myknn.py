df82d22c19a95fb5e68e056f56dd247bfcda99be
bb0p <b@b0p.us>
2018-02-14 14:14:52 -0500

    shorter and more random candidate list

--- myknn.py
+++ myknn.py
@@ -11,11 +11,16 @@ def knn(x,k,e=1e-6):
 	while mean(total_mse)+e<old_mse: # As long as we can improve
 		old_mse=mean(total_mse) 
 		for i in range(len(x)): # improve nn[i,:] for all x[i,:]
-			candidates=list(nn[i]) # Remember current neighbors
+			candidates=[]
 			# Extend candidate list with the neighbors of your neighbors
 			for j in range(len(nn[i])): candidates.extend(nn[nn[i][j]])
+			random.shuffle(candidates) # randomize list
+			candidates=candidates[0:k] # and keep only k
+			# Add k random items for good luck
+			candidates.extend(random.randint(len(nn),size=k))
 			# Exclude yourself!
 			candidates=list(set(filter(lambda a:a!=i,candidates)))
+			candidates.extend(list(nn[i])) # Remember current neighbors
 			# Calculate mse for the candidates
 			list_mse=[mean((x[i]-x[a])**2) for a in candidates]
 			nn[i]=[] # Flush and add new neighbors
