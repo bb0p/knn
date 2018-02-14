07860b0a73c83a6cc7d43f722611823c2f86b821
bb0p <b@b0p.us>
2018-02-13 22:29:21 -0500

    change to function

--- myknn.py
+++ myknn.py
@@ -1,39 +1,23 @@
 from numpy import *
-import glob
 
-k=10
-
-f=glob.glob('features/*.npy')
-v=load(f[0])
-v=zeros((len(v),len(f)))
-for i in range(len(f)): v[:,i]=load(f[i]).flatten()
-x=[]
-r=0
-for i in range(len(v)):
-	l=[]
-	for j in range(k):
-		r=random.randint(len(v))
-		while r==i:
-			r=random.randint(len(v))
-		l.append(r)	
-	x.append(l)	
-total_mse=ones(len(x))
-old_mse=mean(total_mse)+1
-e=0
-while mean(total_mse)<old_mse:
-	old_mse=mean(total_mse)
-	for i in range(len(x)):
-		l=list(x[i])
-		for j in range(len(x[i])): l.extend(x[x[i][j]])
-		l=list(set(filter(lambda a: a!=i,l)))
-		mse=zeros(len(l))		
-		for j in range(len(l)):	mse[j]=mean((v[i]-v[l[j]])**2)
-		sort_mse=argsort(mse)
-		del x[i]
-		x.insert(i,[])
-		for j in range(k): x[i].append(l[sort_mse[j]])
-		total_mse[i]=mean(sort(mse)[0:k])
-		if random.randint(10000)==0: print e,i,mean(total_mse),old_mse-mean(total_mse)
-	print e,i,mean(total_mse),old_mse-mean(total_mse)
-	print '---------------------------------------------'
-	e+=1
+def knn(x,k): 
+	"""Find k-nearest neighbors of x[i,:] for all i"""
+	# Fill nn matrix with references to random neighbors
+	nn=random.randint(len(x),size=(len(x),k)).tolist()
+	total_mse=ones(len(nn)) # Initialize mse
+	old_mse=mean(total_mse)+1
+	while mean(total_mse)<old_mse: # As long as we can improve
+		old_mse=mean(total_mse) 
+		for i in range(len(x)): # improve nn[i,:] for all x[i,:]
+			candidates=list(nn[i]) # Remember current neighbors
+			# Extend candidate list with the neighbors of your neighbors
+			for j in range(len(nn[i])): candidates.extend(nn[nn[i][j]])
+			# Exclude yourself!
+			candidates=list(set(filter(lambda a:a!=i,candidates)))
+			# Calculate mse for the candidates
+			list_mse=[mean((x[i]-x[a])**2) for a in candidates]
+			nn[i]=[] # Flush and add new neighbors
+			nn[i].extend(array(candidates)[argsort(list_mse)])
+			total_mse[i]=mean(sort(list_mse)[0:k])
+		print mean(total_mse),old_mse-mean(total_mse)
+	return nn
